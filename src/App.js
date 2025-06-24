290343e323ac4acc5fb977bd2f70b313c6658183
Soham Vankudre <sohamvankudre@Sohams-MacBook-Pro.local>
2025-06-24 00:20:29 -0700

    Initial commit: React + Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './output.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center text-white text-4xl font-bold">
+      Tailwind is working!
+    </div>
+
+  );
+}
+
+export default App;
